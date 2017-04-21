f42e4254bd1dc05abc2719104bc514a4bac30ed6
kvin1087 <kaustubh87@gmail.com>
2017-04-21 15:26:13 -0500

    Added mongoose and body parser dependencies

--- meanstackapp/app.js
+++ meanstackapp/app.js
@@ -2,10 +2,14 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 var mongoose = require('mongoose');
-
+var bodyParser = require('body-parser');
 var morgan = require('morgan');
 
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
+
+var User = require('./app/models/user');
 
 mongoose.connect('mongodb://localhost:27017/tutorial', function(req,res,error){
     if(error){
@@ -23,6 +27,15 @@ app.get('/', function(req,res){
 
 app.get('/help', function(req,res){
    res.send("Help Page");
+});
+
+app.post('/users', function(req,res){
+    var user = new User();
+    user.username = req.body.username;
+    user.password = req.body.password;
+    user.email = req.body.email;
+    user.save();
+    res.send('User created');
 });
 
 app.listen(port , function(req,res){
