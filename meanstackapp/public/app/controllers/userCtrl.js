2e6af02f1e7da228c11795acca4a61242a72e5a6
kvin1087 <kaustubh87@gmail.com>
2017-04-23 23:20:54 -0500

    Added status notifications

--- meanstackapp/public/app/controllers/userCtrl.js
+++ meanstackapp/public/app/controllers/userCtrl.js
@@ -3,10 +3,12 @@ angular.module('userControllers', [])
 .controller('regCtrl', function ($http) {
     
     this.regUser = function(regData){
-        console.log('form submitted');
-        console.log(this.regData);
+       
+        //console.log(this.regData);
         
-        $http.post('/api/users' , this.regData);
+        $http.post('/api/users' , this.regData).then(function(data){
+            console.log(data);
+        });
     };
 
 });
