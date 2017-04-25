3792db37f9e32361c976d8f1e8b747978d7b19cb
kvin1087 <kaustubh87@gmail.com>
2017-04-25 06:18:10 -0500

    Authentication logic

---
+++ meanstackapp/public/app/services/authServices.js
@@ -0,0 +1,13 @@
+angular.module('authServices', [])
+
+.factory('Auth', function($http){
+   
+    var authFactory = {};
+    
+    authFactory.login = function(regData){
+        return $http.post('/api/users', regData);
+    }
+    
+    return authFactory;
+    
+});
