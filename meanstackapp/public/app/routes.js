093ca54486eb691e26ce16812f780b1aa4e67c90
kvin1087 <kaustubh87@gmail.com>
2017-04-25 06:17:43 -0500

    Added login logice

--- meanstackapp/public/app/routes.js
+++ meanstackapp/public/app/routes.js
@@ -17,6 +17,10 @@ angular.module('appRoutes', ['ngRoute'])
             controllerAs: 'register'
             
         })
+    
+        .when('/login', {
+            templateUrl: 'app/views/pages/users/login.html'
+        })
 
         .otherwise({
             redirectTo: '/'
