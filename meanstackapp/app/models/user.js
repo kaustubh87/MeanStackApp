47724856c13861e800461d6f3118495f16c6893c
kvin1087 <kaustubh87@gmail.com>
2017-04-26 15:54:38 -0500

    Added a new method to check passwords using brcypt hash

--- meanstackapp/app/models/user.js
+++ meanstackapp/app/models/user.js
@@ -37,4 +37,9 @@ UserSchema.pre('save', function(next){
 });
 
 
+UserSchema.methods.comparePassword = function(password){
+    return bcrypt.compareSync(password, this.password);
+};
+
+
 module.exports = mongoose.model('User', UserSchema);
