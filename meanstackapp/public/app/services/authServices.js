0d5af4484e0b6badd6a8d624387ca6a4c900eba1
kvin1087 <kaustubh87@gmail.com>
2017-04-28 22:09:50 -0500

    Added services

--- meanstackapp/public/app/services/authServices.js
+++ meanstackapp/public/app/services/authServices.js
@@ -4,8 +4,8 @@ angular.module('authServices', [])
    
     var authFactory = {};
     
-    authFactory.login = function(regData){
-        return $http.post('/api/users', regData);
+    authFactory.login = function(loginData){
+        return $http.post('/api/authenticate', loginData);
     }
     
     return authFactory;
